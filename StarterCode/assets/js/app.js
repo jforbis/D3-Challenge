cae51cbcb7606a67992a23db0932285585c25377
jforbis <forbisjohn@gmail.com>
2021-02-28 22:10:19 -0600

    save point: was able to read in my data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -7,7 +7,7 @@ let margin = {
     right: 80,
     bottom: 80,
     left: 80
-}
+};
 
 let width = svgwidth - margin.left - margin.right;
 let height = svgheight - margin.top - margin.bottom;
@@ -15,10 +15,18 @@ let height = svgheight - margin.top - margin.bottom;
 let svg = d3
     .select("body")
     .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
+    .attr("height", svgheight)
+    .attr("width", svgwidth);
 
 let chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv("hours-of-tv-watched.csv").then(function(tvData) {
+// reading in my data
+d3.csv("assets/data/data.csv").then(function(data) {
+    console.log(data);
+    // data.forEach(function(info) {
+    //     info.hours = +info.hours;
+    // });
+}).catch(function(error) {
+    console.log(error);
+});
