07ed68286f3ab4383cb522851f7d5196700a984d
jforbis <forbisjohn@gmail.com>
2021-02-28 21:42:17 -0600

    save point: about to render my svg

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+let svgwidth = 960;
+let svgheight = 620;
+
+let margin = {
+    top: 80,
+    right: 80,
+    bottom: 80,
+    left: 80
+}
+
+let width = svgwidth - margin.left - margin.right;
+let height = svgheight - margin.top - margin.bottom;
