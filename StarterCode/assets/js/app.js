f42c9f168e911e99dc294151e8486e3394dce706
jforbis <forbisjohn@gmail.com>
2021-03-01 22:04:50 -0600

    save point: keep getting error that my circlesGroup has already been declared; trying to figure out why.

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -21,27 +21,196 @@ let svg = d3
 let chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// reading in my data
-d3.csv("assets/data/data.csv").then(function(data) {
-    console.log(data);
-    data.forEach(function(info) {
-        info.hours = +info.hours;
+        // Initial Params
+let chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(data, chosenXAxis) {
+// create scales
+let xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+    d3.max(data, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+let bottomAxis = d3.axisBottom(newXScale);
+
+xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+var label;
+
+if (chosenXAxis === "poverty") {
+    label = "In Poverty %:";
+}
+else {
+    label = "Percentage of Smokers:";
+}
+
+var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+    return (`${d.abbr}<br>${label} ${d[chosenXAxis]}`);
     });
-    let barSpacing = 10; 
-    let scaleY = 10; 
 
-    let barWidth = (width - (barSpacing * (data.length - 1))) / data.length;
+circlesGroup.call(toolTip);
 
-    // Create code to build the bar chart using the data.
-    chartGroup.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .classed("bar", true)
-        .attr("width", d => width)
-        // .attr("height", d => d.hours * scaleY)
-        .attr("x", (d, i) => i * (barWidth + barSpacing))
-        // .attr("y", d => chartHeight - d.hours * scaleY);
+circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+})
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+    toolTip.hide(data);
+    });
+
+return circlesGroup;
+}
+// reading in my data
+d3.csv("data.csv").then(function(stateData, err) {
+    console.log(stateData);
+    if (err) throw err;
+
+    // parse data
+    stateData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+    data.smokes = +data.smokes;
+    });
+
+    // xLinearScale function above csv import
+    let xLinearScale = xScale(stateData, chosenXAxis);
+
+    // Create y scale function
+    let yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.healthcare)])
+    .range([height, 0]);
+
+    // Create initial axis functions
+    let bottomAxis = d3.axisBottom(xLinearScale);
+    let leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    let xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+    .call(leftAxis);
+
+    // append initial circles
+    let circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 20)
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+    // Create group for two x-axis labels
+    let labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    let povertyLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "poverty") // value to grab for event listener
+    .classed("active", true)
+    .text("In Poverty (%)");
+
+    var healthcareLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "healthcare") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Lacks Healthcare (%)");
+
+    // todo: come back and figure out if this is correct
+    // append y axis
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Lacks Healthcare (%)");
+
+    // updateToolTip function above csv import
+    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+    console.log(circlesGroup);
+
+    // x axis labels event listener
+    labelsGroup.selectAll("text")
+    .on("click", function() {
+        // get value of selection
+        let value = d3.select(this).attr("value");
+        if (value !== chosenXAxis) {
+
+            // replaces chosenXAxis with value
+        chosenXAxis = value;
+
+            console.log(chosenXAxis)
+
+            // functions here found above csv import
+            // updates x scale for new data
+        xLinearScale = xScale(stateData, chosenXAxis);
+
+            // updates x axis with transition
+        xAxis = renderAxes(xLinearScale, xAxis);
+
+            // updates circles with new x values
+        circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+            // updates tooltips with new info
+        circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+            // changes classes to change bold text
+        if (chosenXAxis === "smokes") {
+            povertyLabel
+            .classed("active", true)
+            .classed("inactive", false);
+            healthcareLabel
+            .classed("active", false)
+            .classed("inactive", true);
+        }
+        else {
+            povertyLabel
+            .classed("active", false)
+            .classed("inactive", true);
+            healthcareLabel
+            .classed("active", true)
+            .classed("inactive", false);
+        }
+        }
+    });
 }).catch(function(error) {
     console.log(error);
 });
